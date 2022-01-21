694ae0225b4f68aa9881241f318ac9122bdcb06f
Batin EVIRGEN <batinevirgen@gmail.com>
2022-01-21 20:46:59 +0300

    Added TailwindCSS and PostCSS.

--- src/pages/index.js
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-
-const IndexPage = () => {
-  return (
-    <div>batin.sh</div>
-  )
-}
-
-export default IndexPage;
---
+++ src/pages/index.tsx
@@ -0,0 +1,10 @@
+import { PageProps } from "gatsby";
+import React from "react";
+
+function IndexPage(props: PageProps) {
+  return (
+    <div>batin.sh</div>
+  )
+}
+
+export default IndexPage;
